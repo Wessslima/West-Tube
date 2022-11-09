a73fa8f479caba8bc0dffe3a054c633b21fc1f8b
unknown <wes_lima@outlook.com>
2022-11-09 19:12:24 -0300

    Implementando Dark Mode

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,20 @@
 import React from "react";
 import config from "../config.json";
 import styled from "styled-components";
-import { CSSReset } from "../src/components/CSSReset";
 import Menu from "../src/components/Menu";
 import { StyledTimeline } from "../src/components/Timeline";
 
 
 function HomePage() {
-    const estilosDaHomePage = {
-        // backgroundColor: "red" 
-    };
+
     const [valorDoFiltro, setValorDoFiltro] = React.useState("");
 
     return (
         <>
-            <CSSReset />
             <div style={{
                 display: "flex",
                 flexDirection: "column",
                 flex: 1,
-                // backgroundColor: "red",
             }}>
                 <Menu valorDoFiltro={valorDoFiltro} setValorDoFiltro={setValorDoFiltro} />
                 <Header />
@@ -33,16 +28,9 @@ function HomePage() {
 
 export default HomePage
 
-// function Menu() {
-//     return (
-//         <div>
-//             Menu
-//         </div>
-//     )
-// }
-
-
 const StyledHeader = styled.div`
+    background-color: ${({theme}) => theme.backgroundLevel1};
+
     img {
         width: 80px;
         height: 80px;
@@ -83,10 +71,7 @@ function Header() {
 }
 
 function Timeline({searchValue, ...propriedades}) {
-    // console.log("Dentro do componente", propriedades.playlists);
     const playlistNames = Object.keys(propriedades.playlists);
-    // Statement
-    // Retorno por expressão
     return (
         <StyledTimeline>
             {playlistNames.map((playlistName) => {
