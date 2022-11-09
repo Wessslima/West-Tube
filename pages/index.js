24c4c042019f7743382f3ebed51630919bf4ebc8
unknown <wes_lima@outlook.com>
2022-11-08 21:12:08 -0300

    first commit

---
+++ pages/index.js
@@ -0,0 +1,121 @@
+import React from "react";
+import config from "../config.json";
+import styled from "styled-components";
+import { CSSReset } from "../src/components/CSSReset";
+import Menu from "../src/components/Menu";
+import { StyledTimeline } from "../src/components/Timeline";
+
+
+function HomePage() {
+    const estilosDaHomePage = {
+        // backgroundColor: "red" 
+    };
+    const [valorDoFiltro, setValorDoFiltro] = React.useState("");
+
+    return (
+        <>
+            <CSSReset />
+            <div style={{
+                display: "flex",
+                flexDirection: "column",
+                flex: 1,
+                // backgroundColor: "red",
+            }}>
+                <Menu valorDoFiltro={valorDoFiltro} setValorDoFiltro={setValorDoFiltro} />
+                <Header />
+                <Timeline searchValue={valorDoFiltro} playlists={config.playlists}>
+                    Conteúdo
+                </Timeline>
+            </div>
+        </>
+    );
+}
+
+export default HomePage
+
+// function Menu() {
+//     return (
+//         <div>
+//             Menu
+//         </div>
+//     )
+// }
+
+
+const StyledHeader = styled.div`
+    img {
+        width: 80px;
+        height: 80px;
+        border-radius: 50%;
+    }
+    .user-info {
+        display: flex;
+        align-items: center;
+        width: 100%;
+        padding: 16px 32px;
+        gap: 16px;
+    }
+`;
+
+const StyledBanner = styled.div`
+        background-color: darkred;
+        height: 200px;
+        background-image: url("https://images.unsplash.com/photo-1511447333015-45b65e60f6d5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=955&q=80");
+`;
+function Header() {
+    return (
+        <StyledHeader>
+            <StyledBanner />
+            {/* <img src="banner" /> */}
+            <section className="user-info">
+                <img src={`https://github.com/${config.github}.png`} />
+                <div>
+                    <h2>
+                        {config.name}
+                    </h2>
+                    <p>
+                        {config.job}
+                    </p>
+                </div>
+            </section>
+        </StyledHeader>
+    )
+}
+
+function Timeline({searchValue, ...propriedades}) {
+    // console.log("Dentro do componente", propriedades.playlists);
+    const playlistNames = Object.keys(propriedades.playlists);
+    // Statement
+    // Retorno por expressão
+    return (
+        <StyledTimeline>
+            {playlistNames.map((playlistName) => {
+                const videos = propriedades.playlists[playlistName];
+                console.log(playlistName);
+                console.log(videos);
+                return (
+                    <section key={playlistName}>
+                        <h2>{playlistName}</h2>
+                        <div>
+                            {videos.filter((video) => {
+                                const titleNormalized = video.title.toLowerCase();
+                                const searchValueNormalized = searchValue.toLowerCase();
+                                return titleNormalized.includes(searchValueNormalized)
+
+                            }).map((video) => {
+                                return (
+                                    <a key={video.url} href={video.url}>
+                                        <img src={video.thumb} />
+                                        <span>
+                                            {video.title}
+                                        </span>
+                                    </a>
+                                )
+                            })}
+                        </div>
+                    </section>
+                )
+            })}
+        </StyledTimeline>
+    )
+}
